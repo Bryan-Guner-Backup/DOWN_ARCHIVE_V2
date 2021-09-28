acae1567b5423ac3220d891dbe9b8aeaec20b195
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-09-28 16:43:44 +0000

    hi

---
+++ LAMBDA_LABS/budget-blocks-ds/routers/admin.py
@@ -0,0 +1,152 @@
+from DB.masterDB import *
+from DB.userDB import *
+import secrets
+from mainhelp import *
+from dotenv import load_dotenv
+from fastapi.templating import Jinja2Templates
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import os
+from fastapi import FastAPI, Request, status, Depends, HTTPException, APIRouter
+
+router = APIRouter()
+
+# Load the file from the path
+load_dotenv()
+
+AUSERNAME = os.getenv("AUSERNAME", default="OOPS")
+PASSWORD = os.getenv("PASSWORD", default="OOPS")
+
+templates = Jinja2Templates(directory="templates")
+
+security = HTTPBasic()
+
+# Function to check that the user is logged in
+
+
+def get_current_username(
+        credentials: HTTPBasicCredentials = Depends(security), tags=["admin"]):
+    correct_username = secrets.compare_digest(credentials.username, "test")
+    correct_password = secrets.compare_digest(credentials.password, "test")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+# End point for main admin interface
+@router.get("/admin", tags=["admin"])
+def admin_main(request: Request,
+               username: str = Depends(get_current_username)):
+    return templates.TemplateResponse("admin_main.html",
+                                      {'request': request})
+
+# Endpoint for reset master confirmation page
+@router.get("/admin/reset_master_confirmation", tags=["admin"])
+def reset_master_confirmation(request: Request):
+    return templates.TemplateResponse("master_confirmation.html",
+                                      {'request': request})
+
+# Endpoint for reset user confirmation page
+@router.get("/admin/reset_user_confirmation", tags=["admin"])
+def reset_user_confirmation(request: Request):
+    return templates.TemplateResponse("user_confirmation.html",
+                                      {'request': request})
+
+# Endpoint to reset the master table
+@router.get("/admin/reset_master/", tags=["admin"])
+def reset_master(request: Request,
+                 username: str = Depends(get_current_username)):
+    resetMaster()
+    message = "The master table was reset"
+    return templates.TemplateResponse("reset_success.html",
+                                      {'request': request,
+                                       'message': message})
+
+# Admin route to edit the master DB table through an interface
+@router.get("/admin/edit_master", tags=["admin"])
+async def testing(request: Request,
+                  Cat: str = 'None',
+                  Plaid_cat: str = 'None',
+                  Destination: str = 'None',
+                  username: str = Depends(get_current_username)):
+
+    # If Value and Destination have values, update the database
+    if (Plaid_cat != 'None') & (Destination != 'None'):
+        # Function that updates the master DB table
+        updateMaster(old_cat=Cat, plaid_cat=Plaid_cat, destination=Destination)
+
+        # Update change log table
+        updateChangeLog(plaid_cat=Plaid_cat, old_BB=Cat, new_BB=Destination)
+
+        # Get the 5 most recent changes in the changelog
+        changes = masterChanges()
+
+        # Redirect to edit_success page which easily allows the admin to return
+        # to the main admin panel or go directly to the admin/db page
+        return templates.TemplateResponse("edit_success.html",
+                                          {'request': request,
+                                           'plaid_cat': Plaid_cat,
+                                           'cat': Cat,
+                                           'destination': Destination,
+                                           'changes': changes})
+    # Pull the current default from the master DB table
+    Dict = masterPull()
+
+    # Use the helper function DictHTML to have 2 dictionaries:
+    # one for displaying readable Plaid categories to the user and one for
+    # using to change the DB later
+    html_dict, spaces_dict = DictHTML(Dict)
+
+    # If a category is chosen, save a zipped version of the two different
+    # lists (readable and useful) of values to 'value_list'
+    if Cat != 'None':
+        value_list = zip(html_dict[Cat], spaces_dict[Cat])
+    else:
+        value_list = 'None'
+
+    # Save the Budget Block categories to the variable 'cats'
+    cats = list(html_dict.keys())
+
+    # Return the admin.html template with the appropriate variables in the
+    # context parameter
+    return templates.TemplateResponse("admin_edit.html",
+                                      {'request': request,
+                                       'cats': cats,
+                                       'Cat': Cat,
+                                       'Dict': Dict,
+                                       'Plaid_cat': Plaid_cat,
+                                       'Destination': Destination,
+                                       'Value_list': value_list})
+
+# Route that allows us to check the current values of the Postgres master table
+@router.get("/admin/db", tags=["admin"])
+async def db(request: Request,
+             username: str = Depends(get_current_username)):
+    master = masterPull()
+    keys = list(master.keys())
+
+    return templates.TemplateResponse("db.html",
+                                      {'request': request,
+                                       'keys': keys,
+                                       'values': master})
+
+# Route that allows us to reset/create the users table
+@router.get("/admin/reset_user", tags=["admin"])
+async def user(request: Request,
+               username: str = Depends(get_current_username)):
+    resetUserTable()
+    message = "The Users table was reset"
+    return templates.TemplateResponse("reset_success.html",
+                                      {'request': request,
+                                       'message': message})
+
+# Route to display all changes in the change log table
+@router.get("/admin/changelog", tags=["admin"])
+async def view_changes(request: Request,
+                       username: str = Depends(get_current_username)):
+    changes = masterChanges(recent=False)
+    return templates.TemplateResponse("master_changes.html",
+                                      {'request': request,
+                                       'changes': changes})
---
+++ LAMBDA_LABS/budget-blocks-ds/routers/posts.py
@@ -0,0 +1,90 @@
+from fastapi import APIRouter
+from transactionhist import *
+from DB.userDB import *
+from census import *
+import time
+
+router = APIRouter()
+
+# End point for the web backend to send transaction history objects and
+# get back the same object w/ BB categories
+@router.post("/transaction/", tags=["transaction"])
+def transaction(trans: TransactionHistory):
+    start_time = time.time()
+
+    # Get the user ID
+    user_id = trans.user_id
+
+    # Get user preferences
+    user_dict = getUser(user_id)
+
+    # Recategorize the transactions
+    request = trans.getCats(cats_dict=getUser(user_id))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    request['request time in seconds'] = (time.time() - start_time)
+
+    return request
+
+# End point for the web team to send a user's location and user id, and
+# receive the average census expenditures of people in the nearest city
+@router.post("/census", tags=["census"])
+async def user_census(census: Census):
+
+    # Get the user ID
+    user_id = census.user_id
+
+    # Get the user's preferences
+    user_dict = getUser(user_id)
+
+    # Create the personalized census dictionary
+    personalized_census = census.census_totals(user_dict=user_dict)
+
+    return personalized_census
+
+# End point for the web team to edit a user's preferences by switching a
+# specific plaid category from one BB category to another
+@router.post("/update_users", tags=["update_users"])
+async def update_users(update: UpdatePreferences):
+    new = update.changePreferences()
+    if new == 0:
+        message = "Updated preferences successfully"
+    return({"message": message})
+
+# Test version of the transaction endpoint where "Income" is a seperate key from the rest of the totals
+# @router.post("/transaction_test/", tags=["transaction"])
+# def transaction(trans: TransactionHistory):
+#     start_time = time.time()
+
+#     # Get the user ID
+#     user_id = trans.user_id
+
+#     # Get user preferences
+#     user_dict = getUser(user_id)
+
+#     # Recategorize the transactions
+#     request = trans.getCats(cats_dict=getUser(user_id))
+
+#     # Create a new seperate key for income to test Adam's solution and delete
+#     # the old one
+#     request['Income'] = request['totals']['Income']
+#     del request['totals']['Income']
+
+#     print("--- %s seconds ---" % (time.time() - start_time))
+
+#     request['request time in seconds'] = (time.time() - start_time)
+
+#     return request
+
+@router.post("/reset_user", tags=['reset_user'])
+def reset_user(user: User):
+    message = user.reset_user_cats()
+
+    return({"message": message})
+
+@router.post("/delete_user", tags=['delete_user'])
+def delete_user(user: User):
+    message = user.delete_user()
+
+    return({"message": message})
