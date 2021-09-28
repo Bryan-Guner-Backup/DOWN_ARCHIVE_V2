dc481b42828c285339258247fc84d35df1306da3
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-09-28 16:41:39 +0000

    hi

---
+++ LAMBDA_LABS/bio-bid-be/prisma/generated/prisma-client/index.js
@@ -0,0 +1,61 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Company",
+    embedded: false
+  },
+  {
+    name: "Claim",
+    embedded: false
+  },
+  {
+    name: "ServiceItem",
+    embedded: false
+  },
+  {
+    name: "Service",
+    embedded: false
+  },
+  {
+    name: "SpecialtyItem",
+    embedded: false
+  },
+  {
+    name: "Specialty",
+    embedded: false
+  },
+  {
+    name: "Region",
+    embedded: false
+  },
+  {
+    name: "Therapeutic",
+    embedded: false
+  },
+  {
+    name: "CompanySize",
+    embedded: false
+  },
+  {
+    name: "Phase",
+    embedded: false
+  },
+  {
+    name: "Bid",
+    embedded: false
+  },
+  {
+    name: "Study",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/biobid-team/production/prod`
+});
+exports.prisma = new exports.Prisma();
