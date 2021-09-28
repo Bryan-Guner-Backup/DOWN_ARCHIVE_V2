1c08b7cdee2b753918e779621258f6f098a97949
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-09-28 16:53:42 +0000

    hi

---
+++ LAMBDA_LABS/deprecated-labs32-human-rights-first-asylum-be-b/api/dal/users.js
@@ -0,0 +1,71 @@
+const db = require('../../data/Pool');
+const sqlLoader = require('sql-loader');
+
+const sql = sqlLoader('../sql');
+
+async function getUsers() {
+  const { rows } = await db.query(sql.users.get_users);
+  return rows;
+}
+
+async function getUserById(id) {
+  const { rows } = await db.query(sql.users.get_user_by_id, [id]);
+  return rows;
+}
+
+async function getUserByEmail(email) {
+  const { rows } = await db.query(sql.users.get_user_by_email, [email]);
+  return rows;
+}
+
+async function createUser({
+  firstName,
+  lastName,
+  password,
+  email,
+  permissions_flag,
+  date_created,
+}) {
+  const { rows } = await db.query(sql.users.create_user, [
+    firstName,
+    lastName,
+    password,
+    email,
+    permissions_flag,
+    date_created,
+  ]);
+  return rows;
+}
+
+async function updateUser({
+  id,
+  firstName,
+  lastName,
+  password,
+  email,
+  permissions_flag,
+}) {
+  const { rows } = await db.query(sql.users.update_user, [
+    id,
+    firstName,
+    lastName,
+    password,
+    email,
+    permissions_flag,
+  ]);
+  return rows[0];
+}
+
+async function deleteUser(id) {
+  const { rows } = await db.query(sql.users.delete_user, [id]);
+  return rows;
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  getUserByEmail,
+  createUser,
+  updateUser,
+  deleteUser,
+};
