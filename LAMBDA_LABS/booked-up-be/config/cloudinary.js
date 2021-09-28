3e25a120d7dba27ef0505eaf6b94ebcf2b1d5e3a
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-09-28 16:42:20 +0000

    hi

---
+++ LAMBDA_LABS/booked-up-be/config/cloudinary.js
@@ -0,0 +1,7 @@
+const cloudinary = require("cloudinary");
+
+module.exports = cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
