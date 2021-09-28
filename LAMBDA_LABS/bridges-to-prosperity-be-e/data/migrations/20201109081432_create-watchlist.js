c8532cddf6707a2c1ffd1176fc6ec054b817ca46
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-09-28 16:42:54 +0000

    hi

---
+++ LAMBDA_LABS/bridges-to-prosperity-be-e/data/migrations/20201109081432_create-watchlist.js
@@ -0,0 +1,33 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('watchlist', (list) => {
+      list.increments();
+      list.string('title', 50);
+      list
+        .string('profile_id')
+        .references('id')
+        .inTable('profiles')
+        .unique()
+        .notNullable();
+      list.text('notes', 255);
+    })
+    .createTable('watchlist_bridges', (bridge) => {
+      bridge.increments();
+      bridge
+        .string('project')
+        .references('project_code')
+        .inTable('bridges')
+        .notNullable();
+      bridge
+        .string('list_id')
+        .references('profile_id')
+        .inTable('watchlist')
+        .notNullable();
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('watchlist_bridges')
+    .dropTableIfExists('watchlist');
+};
